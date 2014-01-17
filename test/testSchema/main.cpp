344279b6b88e7ddd6315f76c39e907c526e174d6
qiwei <qiweibo1984@126.com>
2014-01-17 19:53:00 +0800

    添加了xml Schema 测试用例

---
+++ test/testSchema/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>    
+#include "libxml/tree.h"    
+#include "libxml/parser.h"    
+#include "libxml/xmlschemas.h"    
+        
+/****************************************************  
+    @describle   应用XML Schema模板文件验证案例文档 
+    @param schema_filename  模式文件  
+    @param xmldoc           XML格式的案例文档 
+    @retval  ==0  验证成功 
+            >0  验证失败  
+****************************************************/  
+int is_valid(const char *schema_filename, const char *xmldoc) {  
+    xmlDocPtr doc;    
+    //doc = xmlReadFile(xmldoc, NULL, XML_PARSE_NONET|XML_PARSE_NOENT);  
+	doc = xmlParseFile(xmldoc);
+    if ( NULL == doc) {    
+        std::cout << "读取XML文档错误"<<std::endl;  
+        return -1;    
+    }      
+   
+    xmlSchemaParserCtxtPtr parser_ctxt = xmlSchemaNewParserCtxt(schema_filename);  
+    if (NULL == parser_ctxt) {  
+		std::cout << "读取Schema错误" << std::endl;  
+        return -1;  
+    }      
+	xmlSchemaSetParserErrors(parser_ctxt,
+		(xmlSchemaValidityErrorFunc) fprintf,
+		(xmlSchemaValidityWarningFunc) fprintf,
+		stderr);
+	
+
+    xmlSchemaPtr schema = xmlSchemaParse(parser_ctxt);  
+    if (schema == NULL) {    
+        return -1;    
+    }      
+	xmlSchemaFreeParserCtxt(parser_ctxt);
+
+    xmlSchemaValidCtxtPtr valid_ctxt = xmlSchemaNewValidCtxt(schema);  
+    if (NULL == valid_ctxt) {  
+        return -1;    
+    }   
+
+	xmlSchemaSetValidErrors(valid_ctxt,
+		(xmlSchemaValidityErrorFunc) fprintf,
+		(xmlSchemaValidityWarningFunc) fprintf,
+		stderr);
+
+    int ret = xmlSchemaValidateDoc(valid_ctxt,doc);  
+	if (ret == 0) {
+		printf("%s validates\n", xmldoc);
+	} else if (ret > 0) {
+		printf("%s fails to validate\n", xmldoc);
+	} else {
+		printf("%s validation generated an internal error\n",
+			xmldoc);
+	}
+    xmlSchemaFreeValidCtxt(valid_ctxt);  
+    xmlSchemaFree(schema);  
+      
+    return ret;  
+} 
+int main(void)
+{
+	return is_valid("scm.xsd","./StateMachine.xml");
+}
