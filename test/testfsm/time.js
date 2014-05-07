71853393bc403ba5dfaf4da94fbd5e267af3bc48
qiwei <qiweibo1984@126.com>
2014-05-07 10:25:29 +0800

    添加了timer.js 文件，是一些实现的JavaScript 时间函数。

---
+++ test/testfsm/time.js
@@ -0,0 +1,17 @@
+function getDay(){
+	var now = new Date();
+	return now.getDay();
+}
+
+function getHour(){
+	var now = new Date();
+	return now.getHours();
+}
+function getMinute(){
+	var now = new Date();
+	return now.getMinutes()
+}
+function getSecond(){
+	var now = new Date();
+	return now.getSeconds();
+}
