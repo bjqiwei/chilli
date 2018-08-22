5061c5fd341cad940ac9eedbf55e7d433530d855
bjqiwei <qiweibo1984@126.com>
2018-08-22 18:50:11 +0800

    sip_gateway 表添加了defaultcaller 字段

--- src/FreeSWITCH/sip_gateway.sql
+++ src/FreeSWITCH/sip_gateway.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50556
 File Encoding         : 65001
 
-Date: 2018-07-27 16:40:41
+Date: 2018-08-22 18:48:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,5 +24,6 @@ CREATE TABLE `sip_gateway` (
   `ip` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '网关IP地址',
   `port` int(11) DEFAULT '5060' COMMENT '网关端口',
   `name` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '网关名称',
+  `defaultcaller` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '默认显示的主叫号码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='网关配置表，配置外部SIP网关';
